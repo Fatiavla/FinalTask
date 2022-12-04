3c3b29ba1bf615e3616d451a913688b9d4b9693f
Fatiavla <bioafc@mail.ru>
2022-12-04 12:25:37 +0300

    task complited

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿string [] array1 = new string[5] {"hello", "2", "world", ":-)", "12"};
+string [] array2 = new string [array1.Length];
+
+void GetArray(string[]array1, string[]array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <=3)
+        array2[count]=array1[i];
+        count ++;
+    }
+}
+
+
+void PrintArray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+GetArray(array1, array2);
+PrintArray(array2);
